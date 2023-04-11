9f3423c7e8c0d70d168e27d05a831a1b47b43976
Herenyon <rikitur17@gmail.com>
2023-04-11 19:35:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$password_lenght = $_GET['number'];
+var_dump($password_lenght);
+
+function randomPassword($password_lenght) {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!£$%^&*';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < $password_lenght; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+$password = randomPassword($password_lenght);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        
+        <input type="number" name="number" placeholder="inserisci un numero">
+        <button>Invia</button>
+    </form>
+    <div> <?php echo $password ?></div>
+</body>
+</html>
